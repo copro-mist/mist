c68293633d3b1a969081c7bf8ab55dbe9689327e
jreichwein <julian.reichwein@cofinpro.de>
2017-09-08 12:12:08 +0200

    folder structure

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,15 @@ var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello mist!');
+  console.log(req.param);
+});
+
+app.post('/', (req, res)=> {
+    console.log(req.params);
+});
+
+app.get('/newentry', (req, res) => {
+    res.send('NEUER EINTRAG WURDE GEMACHT');
 });
 
 app.listen(3000, function () {
